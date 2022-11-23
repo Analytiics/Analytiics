0720d137f648574ea373005f9d4f232044f7b907
Analytiics <118388167+Analytiics@users.noreply.github.com>
2022-11-23 10:30:33 +0530

    Add Initial Commit

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var owl = $('.owl-carousel');
 owl.owlCarousel({
  loop: true,
  smartSpeed: 80,
+ responsiveBaseElement: 'body',
  responsive: {
   0: {
    items: 1
   },
   600: {
+   items: 1
+  },
+  960: {
    items: 2
   },
-  1000: {
-   items: 4
+  1200: {
+   items: 3
   }
  }
 });
