c4b2e27b7e5cfe968da915ae1d0e116755b71df0
Analytiics <118388167+Analytiics@users.noreply.github.com>
2023-03-15 23:29:17 +0530

    Add Initial Commit

--- ASSETS/app.js
+++ ASSETS/app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 90,
+        "value": 95,
         "density": {
           "enable": true,
           "value_area": 800
