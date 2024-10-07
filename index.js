9956924ddb62e1399bf62270b6881a296fe89860
Analytiics <118388167+Analytiics@users.noreply.github.com>
2024-10-07 12:58:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
- loop: false,
- smartSpeed: 80,
- slideTransition: 'linear',
- responsiveBaseElement: 'body',
- responsive: {
-  0: {
-   items: 1
-  },
-  600: {
-   items: 1
-  },
-  960: {
-   items: 2
-  },
-  1200: {
-   items: 3
+  loop: false,
+  smartSpeed: 80,
+  slideTransition: 'linear',
+  responsiveBaseElement: 'body',
+  margin: 20,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    960: {
+      items: 2
+    },
+    1200: {
+      items: 3
+    }
   }
- }
 });
